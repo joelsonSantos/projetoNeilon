778085c664d16c4f24c5d0bcc9fae8e5df061435
Laiender Sebastião Camargos de Morais <laiender.morais@gmail.com>
2019-01-17 14:24:25 -0200

    funcionalidade da opcao salvar

--- src/DAO/JogadorDAO.java
+++ src/DAO/JogadorDAO.java
@@ -5,6 +5,7 @@
  */
 package DAO;
 
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.GetOpt;
 import controle.Jogador;
 import controle.Pessoa;
 import java.sql.Statement;
@@ -22,7 +23,7 @@ public class JogadorDAO {
         try{
             Conexao conect = new Conexao();
             Statement st = (Statement) conect.getSt();
-            st.execute("insert into pessoa(nome, sobrenome, "
+            st.execute("insert into pessoa(nome, sobrenome,  "
                     + "nascionalidade, rg, cpf, dataNascimento, escolaridade,"
                     + "instituicao) "
                     + "values('" + jogador.getNome()+ "','"
@@ -35,6 +36,34 @@ public class JogadorDAO {
                             + jogador.getInstituicao() +"'+'"
                     
                     +"')");
+            
+            st.execute("insert into endereco(rua, numero, bairro, cidade, "
+                    + "estado, pais, complemento, cep)"
+                    + " values('"+jogador.getEndereco().getRua()+"','"
+                        +jogador.getEndereco().getNumero() +"','"
+                        +jogador.getEndereco().getBairro() +"','" 
+                        +jogador.getEndereco().getCidade() +"','"
+                        +jogador.getEndereco().getEstado() +"','"
+                        +jogador.getEndereco().getPais() +"','"
+                        +jogador.getEndereco().getComplemento() +"','"
+                        +jogador.getEndereco().getCep() + "','"
+                    +"')");
+            
+            st.execute("insert into jogador (categoria, nomeEmpresario, posicao"
+                    + "peso, altura) "
+                    + "value ('"+jogador.getCategoria() +"','"
+                        +jogador.getNomeEmpresario() +"','"
+                        +jogador.getPosicao() +"','"
+                        +jogador.getPeso() +"','"
+                        +jogador.getAltura() +"','"
+                    +"')");
+            
+            st.execute("insert into contatos (telefoneResidencial, celular, email)"
+                    + "values('"+jogador.getContato().getTelefoneResidencial() +"','"
+                        + jogador.getContato().getCelular()+"','"
+                        +jogador.getContato().getEmail()+"','"
+                    +"')");
+            
             
         } catch (Exception e){
             System.out.println("Problemas Ocorreram");
